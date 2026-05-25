9f061239e3fdd491dd276002599f0539d0a29e4e
19inayat <inayat_hc@hotmail.com>
2026-05-25 12:42:01 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.5.4")
 
     implementation("androidx.compose.material3:material3:1.1.2")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
