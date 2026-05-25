e552b9b540dfd123a6077534e2845359d64ca1b2
19inayat <inayat_hc@hotmail.com>
2026-05-25 12:09:44 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,29 @@
 pluginManagement {
+
     repositories {
+
         google()
+
         mavenCentral()
+
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositoriesMode.set(
+        RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+
     repositories {
+
         google()
+
         mavenCentral()
     }
 }
+
 rootProject.name = "NamazDashboard"
+
 include(":app")
